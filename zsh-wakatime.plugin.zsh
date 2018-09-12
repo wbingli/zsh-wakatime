3dbb14a2519bf18c0c6813c776c0ea943a00bcf4
Paul Esson <paul.esson@gmail.com>
2018-09-12 17:13:02 +1000

    feat: don\'t add project name if we don't know it, let wakatime figure it out

--- zsh-wakatime.plugin.zsh
+++ zsh-wakatime.plugin.zsh
@@ -1,27 +1,32 @@
 # wakatime for zsh
 
 # hook function to send wakatime a tick
+hasgit=`type "git">/dev/null 2>&1`$?
+typeset -A dirmap
 send_wakatime_heartbeat() {
     entity=$(waka_filename);
-    project=$(git config --local remote.origin.url 2> /dev/null|sed -n 's#.*/\([^.]*\)\.git#\1#p');
+    project=$(waka_projectname);
+    if [ $project ]; then 
+       projectArg="--project ${project}"
+    fi;
     if [ "$entity" ]; then
-        (wakatime --write --plugin "zsh-wakatime/0.0.1" --entity-type app --project "${project:-Terminal}" --entity "$entity"> /dev/null 2>&1 &)
+        (wakatime --write --plugin "zsh-wakatime/0.0.1" --entity-type app ${projectArg} --entity "$entity"> /dev/null 2>&1 &)
     fi
 }
-
+waka_projectname() {
+    if [ $hasgit ]; then
+        gitproject=$(git config --local remote.origin.url 2> /dev/null|sed -n 's#.*/\([^.]*\)\.git#\1#p')
+        echo ${gitproject}
+    fi
+}
 # generate text to report as "filename" to the wakatime API
 waka_filename() {
     if [ "x$WAKATIME_USE_DIRNAME" = "xtrue" ]; then
         # just use the current working directory
         echo "$PWD"
     else
-	    cmd="$history[$((HISTCMD-1))]" | cut -d ' ' -f1
-	    if [ cmd = "yarn" ]; then
-		    echo "$history[$((HISTCMD-1))]" | cut -d ' ' -f1,2
-	    else	
 	    # only command without arguments to avoid senstive information
-            echo $cmd
-        fi
+        echo "$history[$((HISTCMD-1))]" | cut -d ' ' -f1
     fi
 }
 
