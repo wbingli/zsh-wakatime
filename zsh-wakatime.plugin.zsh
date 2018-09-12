083f35adcd2fe542594c6d341a29fafbde8928fb
Paul Esson <paul.esson@gmail.com>
2018-09-12 13:31:24 +1000

    feat: show more commands if using yarn, add the project from git

--- zsh-wakatime.plugin.zsh
+++ zsh-wakatime.plugin.zsh
@@ -3,8 +3,9 @@
 # hook function to send wakatime a tick
 send_wakatime_heartbeat() {
     entity=$(waka_filename);
+    project=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p');
     if [ "$entity" ]; then
-        (wakatime --write --plugin "zsh-wakatime/0.0.1" --entity-type app --project Terminal --entity "$entity"> /dev/null 2>&1 &)
+        (wakatime --write --plugin "zsh-wakatime/0.0.1" --entity-type app --project "${project:-Terminal}" --entity "$entity"> /dev/null 2>&1 &)
     fi
 }
 
@@ -14,8 +15,13 @@ waka_filename() {
         # just use the current working directory
         echo "$PWD"
     else
-        # only command without arguments to avoid senstive information
-        echo "$history[$((HISTCMD-1))]" | cut -d ' ' -f1
+	    cmd="$history[$((HISTCMD-1))]" | cut -d ' ' -f1
+	    if [ cmd = "yarn" ]; then
+		    echo "$history[$((HISTCMD-1))]" | cut -d ' ' -f1,2
+	    else	
+	    # only command without arguments to avoid senstive information
+            echo $cmd
+        fi
     fi
 }
 
